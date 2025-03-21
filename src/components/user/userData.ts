99a5b85075f8b113663dd2ec5b6e90245a3420cc
Muthukumaran <muthukumaran6967@gmail.com>
2025-03-21 17:38:02 +0530

    [ MK ] - remove all unwanted files

--- src/components/user/userData.ts
+++
@@ -1,6 +0,0 @@
-export const userProfileData = {
-    name: 'Muthukumaran',
-    phone: '8838336941',
-    address: 'North Street',
-    pin: 234235,
-  };
--- src/routers/routes.tsx
+++ src/routers/routes.tsx
@@ -1,26 +1,17 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MarketDashboard from "../components/MarketDashboard";
-import NotFound from "../components/NotFound";
-import Sidebar from "../components/MenuBar";
 
-
-export const AppRouter: React.FC = () => {
+ const AppRouter: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={
-          <>
-            <Sidebar/>
-            <div className="container mx-auto p-4">
-              <MarketDashboard />
-            </div>
-          </>
-        } />
-        <Route path="/not-found" element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
 };
 
+// Example components
+const Home: React.FC = () => <h1>Home Page</h1>;
+const About: React.FC = () => <h1>About Page</h1>;
 export default AppRouter;
